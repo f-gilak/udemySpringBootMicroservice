267354f64ad7d6d34df8d367c46e772c81bc4d6e
f-gilak <63143804+f-gilak@users.noreply.github.com>
2024-11-03 00:45:06 +0330

    first commit eureka server and two eureka client

---
+++ photoAppDiscoveryService/PhotoAppApiUsers/src/main/java/com/example/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -0,0 +1,16 @@
+package com.example.photoapp.api.users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PhotoAppApiUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppApiUsersApplication.class, args);
+	}
+
+}
